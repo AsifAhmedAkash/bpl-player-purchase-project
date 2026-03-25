4b6dbcbe9eca7c759305fd4873b2006618118a50
Asif Ahmed <asifahmedakash32@gmail.com>
2026-03-25 23:34:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <h2 className='p-3'>hello react</h2>
+    </>
+  )
+}
+
+export default App
